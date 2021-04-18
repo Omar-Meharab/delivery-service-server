3a4292015855e8172387f04a807ebe4f584e0911
Omar Meharab <omarmeharab81@gmail.com>
2021-04-18 22:11:39 +0600

    alhamdulillah done

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ admin.initializeApp({
 
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) =>{
-    res.send("hello delivery")
+app.get('/', (req, res) => {
+  res.send("hello delivery")
 })
 
 
@@ -34,83 +34,104 @@ client.connect(err => {
   const servicesCollection = client.db("deliveryService").collection("services");
   const feedbacksCollection = client.db("deliveryService").collection("feedbacks");
   const ordersCollection = client.db("deliveryService").collection("orders");
-  
-    app.get('/services', (req, res) =>{
-      servicesCollection.find({})
-      .toArray( (err, documents) => {
+  const adminsCollection = client.db("deliveryService").collection("admins");
+
+  app.post('/addAdmin', (req, res) => {
+    const admin = req.body;
+    adminsCollection.insertOne(admin)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
+  app.get('/admins', (req, res) => {
+    adminsCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents);
       })
-    })
-    app.get('/feedbacks', (req, res) =>{
-      feedbacksCollection.find({})
-      .toArray( (err, documents) => {
+  })
+  app.get('/services', (req, res) => {
+    servicesCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents);
       })
-    })
-
-    app.get('/service/:id', (req, res) => {
-      servicesCollection.find({_id: ObjectId(req.params.id)})
-      .toArray( (err, documents) =>{
-        res.send(documents[0]);
+  })
+  app.get('/feedbacks', (req, res) => {
+    feedbacksCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
       })
-    })
-    
-    app.post('/addServices', (req, res) =>{
-      const book = req.body;
-      servicesCollection.insertOne(book)
-      .then(result => {
-        res.send(result.insertedCount > 0)
-      })
-    })
-    app.post('/addFeedbacks', (req, res) =>{
-      const book = req.body;
-      feedbacksCollection.insertOne(book)
-      .then(result => {
-        res.send(result.insertedCount > 0)
-      })
-    })
-
-    app.post('/addOrder', (req, res) => {
-      const newOrder = req.body;
-      ordersCollection.insertOne(newOrder)
-          .then(result => {
-              res.send(result.insertedCount > 0);
-          })
   })
 
-    app.delete('/deleteService/:id', (req, res) => {
-      servicesCollection.deleteOne({_id: ObjectId(req.params.id)})
+  app.get('/service/:id', (req, res) => {
+    servicesCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
+
+  app.post('/addServices', (req, res) => {
+    const service = req.body;
+    servicesCollection.insertOne(service)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+  app.post('/addFeedbacks', (req, res) => {
+    const feedback = req.body;
+    feedbacksCollection.insertOne(feedback)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.post('/addOrder', (req, res) => {
+    const newOrder = req.body;
+    ordersCollection.insertOne(newOrder)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+      })
+  })
+
+  app.delete('/deleteService/:id', (req, res) => {
+    servicesCollection.deleteOne({ _id: ObjectId(req.params.id) })
       .then(result => {
         res.send(result.deletedCount > 0);
       })
-    })
+  })
 
-    app.get('/orders', (req, res) => {
-      const bearer = req.headers.authorization;
-      if (bearer && bearer.startsWith('Bearer ')) {
-          const idToken = bearer.split(' ')[1];
-          // idToken comes from the client app
-          admin.auth().verifyIdToken(idToken)
-              .then((decodedToken) => {
-                  const tokenEmail = decodedToken.email;
-                  const queryEmail = req.query.email;
-                  if (tokenEmail == queryEmail) {
-                      ordersCollection.find({ email: queryEmail })
-                          .toArray((err, documents) => {
-                              res.status(200).send(documents);
-                          })
-                  }
-                  else {
-                      res.status(401).send('unauthorized access')
-                  }
+  app.get('/orders', (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const idToken = bearer.split(' ')[1];
+      // idToken comes from the client app
+      admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            ordersCollection.find({ email: queryEmail })
+              .toArray((err, documents) => {
+                res.status(200).send(documents);
               })
-              .catch((error) => {
-                  res.status(401).send('unauthorized access');
-              });
-      }
-      else {
+          }
+          else {
+            res.status(401).send('unauthorized access')
+          }
+        })
+        .catch((error) => {
           res.status(401).send('unauthorized access');
-      }
+        });
+    }
+    else {
+      res.status(401).send('unauthorized access');
+    }
+  })
+  app.post('/isAdmin', (req, res) => {
+    const email = req.body.email;
+    adminsCollection.find({ email: email })
+      .toArray((err, admins) => {
+        res.send(admins.length > 0);
+      })
   })
 
 });
